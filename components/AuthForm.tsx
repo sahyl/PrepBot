a2b4f755945d5de77fd3bc215bc42c4c58b1d9bb
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2025-04-03 18:30:21 +0530

    prepbot

---
+++ components/AuthForm.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import React from "react";
+
+import { Button } from "./ui/button";
+import { Form } from "./ui/form";
+import Image from "next/image";
+import Link from "next/link";
+import { toast } from "sonner";
+import FormField from "./FormField";
+import { useRouter } from "next/navigation";
+
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name:
+      type === "sign-up" ? z.string().min(3).max(13) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(5),
+  });
+};
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const router = useRouter();
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if (type === "sign-up") {
+        toast.success("Account created successfully.Please sign in.");
+        router.push("/sign-in");
+      } else {
+        toast.success("Sign in successfull.");
+        router.push("/")
+
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`There was an error ${error}`);
+    }
+  }
+
+  const isSignin = type === "sign-in";
+
+  return (
+    <div className="card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+          <h2 className="text-primary-100">PrepBot</h2>
+        </div>
+        <h3 className="flex flex-row justify-center" >Practise job interviews with AI</h3>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6 mt-4 form"
+          >
+            {!isSignin && (
+              <FormField
+                control={form.control}
+                name={"name"}
+                label="Name"
+                placeholder="Enter your Name"
+              />
+            )}
+            <FormField
+              control={form.control}
+              name={"email"}
+              label="Email"
+              placeholder="Enter your Email"
+              type="email"
+            />
+
+            <FormField
+              control={form.control}
+              name={"password"}
+              label="Password"
+              placeholder="Enter your Password"
+              type="password"
+            />
+            <Button className="btn" type="submit">
+              {isSignin ? "Sign-in" : "Create an account"}
+            </Button>
+          </form>
+        </Form>
+        <p className="text-center">
+          {isSignin ? "No account yet?" : "Have an account already"}
+
+          <Link
+            href={!isSignin ? "/sign-in" : "/sign-up"}
+            className="font-bold text-user-primary ml-1"
+          >
+            {!isSignin ? "Sign-in" : "Sign-up"}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
