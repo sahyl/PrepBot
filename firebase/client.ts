c70c48c14003a59a030e8cba04886aee475305ec
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2025-04-07 20:42:15 +0530

    env

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCO5m1Q1JCdNks75QmaWnUC72OpiTRkbhQ",
-  authDomain: "prepbot-sahil101.firebaseapp.com",
-  projectId: "prepbot-sahil101",
-  storageBucket: "prepbot-sahil101.firebasestorage.app",
-  messagingSenderId: "1088357559244",
-  appId: "1:1088357559244:web:6dee57e0f0edbb02becb69",
-  measurementId: "G-HXNGFKXHZS",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
